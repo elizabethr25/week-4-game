69926ce2a56099a702019a1507abf9efb6c338fb
Elizabeth Richardson <emrichardson820@gmail.com>
2018-03-24 17:01:48 -0700

    updates, images, code

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//Document ready tag
+
+//Declare global variables: wins, losses, random numbers? 
+
+var 
+
+//When page loads, put random number in Random Number div
+
+//Assign hidden random numbers to each of the crystals
+
+//When a crystal is clicked, add it's random number to the "your score" section
+
+//When another crystal is cicked, add it to the total of "your score"
+
+//If the total of "your score" equals the random number, the player wins, "You won!" is displayed 1 is added to the wins section under score
+
+//If the total of "your score" exceeds the random number, the player loses, "You lost!" is displayed and 1 is added to the losses section under score
+
+//After the game is over reset the game:
+
+//Assign new random numbers to the crystals 
+
+//Generate a new random number
+
+//Reset "your score" to zero
