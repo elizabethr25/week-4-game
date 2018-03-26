17a8e09d53a6887ca42dc7ae389c47b5a90702fa
Elizabeth Richardson <emrichardson820@gmail.com>
2018-03-26 12:46:30 -0700

    new js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,132 @@
 //Document ready tag
+$(document).ready(function (){
+//$(document).ready(function() {
 
 //Declare global variables: wins, losses, random numbers? 
+var wins = 0;
+var losses = 0;
+var yourscore = 0;
 
-var 
-
-//When page loads, put random number in Random Number div
-
+//When the page loads, pput random number in Random Number div
+   
+    var randomnumber = parseInt(Math.floor(Math.random()*101)+9);
+    console.log(randomnumber);
+       
+    $("#randomnum").text(randomnumber);
+    
 //Assign hidden random numbers to each of the crystals
+
+    var blue = parseInt(Math.floor(Math.random()*12)+1)
+    console.log("Blue = " + blue);
+
+    var green = parseInt(Math.floor(Math.random()*12)+1)
+    console.log("Green = " + green);
+
+    var pink = parseInt(Math.floor(Math.random()*12)+1)
+    console.log ("Pink = " + pink);
+
+    var purple = parseInt(Math.floor(Math.random()*12)+1)
+    console.log ("Purple = " + purple);
+
+
+//After the game is over reset the game:
+    function reset () {
+        yourscore = 0;
+        $("#yourscore").text(yourscore);
+        randomnumber = parseInt(Math.floor(Math.random()*101)+9);
+        $("#randomnum").text(randomnumber);
+        console.log ("New random number: " + randomnumber);        
+        blue = parseInt(Math.floor(Math.random()*12)+1);
+        console.log("New blue number: " + blue);
+        green = parseInt(Math.floor(Math.random()*12)+1);
+        console.log ("New green number: " + green);
+        pink = parseInt(Math.floor(Math.random()*12)+1);
+        console.log ("New pink number: " + pink);
+        purple = parseInt(Math.floor(Math.random()*12)+1);
+        console.log ("New purple number: " + purple);
+    }
 
 //When a crystal is clicked, add it's random number to the "your score" section
 
-//When another crystal is cicked, add it to the total of "your score"
 
-//If the total of "your score" equals the random number, the player wins, "You won!" is displayed 1 is added to the wins section under score
 
-//If the total of "your score" exceeds the random number, the player loses, "You lost!" is displayed and 1 is added to the losses section under score
+    $("#blue-crystal").click(function() {
+        yourscore = yourscore + blue;
+        $("#yourscore").text(yourscore);
+        
+        if (yourscore === randomnumber) {
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            console.log(wins);
+            reset();
+        } else if (yourscore > randomnumber) {
+            losses ++;
+            $("#losses").html("Losses: " + losses);
+            reset();
+        }
+    })
 
-//After the game is over reset the game:
+    $("#green-crystal").click(function() {
+        yourscore = yourscore + green;
+        $("#yourscore").text(yourscore);
+
+        if (yourscore === randomnumber){
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            console.log(wins);
+            reset();
+        } else if (yourscore > randomnumber){
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            reset();
+        }
+    })
+
+    $("#pink-crystal").click(function() {
+        yourscore = yourscore + pink;
+        $("#yourscore").text(yourscore);
+
+        if (yourscore === randomnumber){
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            console.log(wins);
+            reset();
+        } else if (yourscore > randomnumber){
+            losses++;
+            $("#losses").html("Losses " + losses);
+            reset();
+        }
+    })
+
+    $("#purple-crystal").click(function() {
+        yourscore = yourscore + purple;
+        $("#yourscore").text(yourscore);
+
+        if (yourscore === randomnumber){
+            wins++;
+            $("wins").html("Wins: " + wins);
+            console.log(wins);
+            reset();
+        } else if (yourscore > randomnumber){
+            losses++;
+            $("losses").html("Losses: " + losses);
+            reset();
+        }
+    })
+
+
+
+
+
+
+
+
+
 
 //Assign new random numbers to the crystals 
 
 //Generate a new random number
 
 //Reset "your score" to zero
+
+})
